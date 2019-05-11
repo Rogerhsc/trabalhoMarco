1f6b4da817c1c988fea13982969e51bd804242ee
Roger <rogerhenrique.souza@hotmail.com>
2019-05-10 22:10:18 -0300

    trabalhoMarco

---
+++ Projetos/java/com/example/trabalhoMarco/TrabalhoMarcoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.trabalhoMarco;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrabalhoMarcoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ Projetos/resources/static/js/funcoes.js
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+const IsEmail = (email) => {
+	
+	var er = new RegExp(/^[A-Za-z0-9_\-\.]+@[A-Za-z0-9_\-\.]{2,}\.[A-Za-z0-9]{2,}(\.[A-Za-z0-9])?/);
+	
+	if(typeof(email) == "string")
+	{		
+		if(er.test(email)){
+			console.log("is Email");
+			document.getElementById('loginButton').disabled = false
+			}
+		else{
+			console.log("is not Email");
+			document.getElementById('loginButton').disabled = true
+		}
+	}
+	else if(typeof(email) == "object"){		
+		if(er.test(email.value)){ 				
+			console.log("is not Email"); 				
+			}	
+	}else{		
+		console.log("is not Email");
+	}
+}
+
+const passWordValidation = (pass) => {
+	
+	if(pass.length < 7){
+		document.getElementById('loginButton').disabled = true
+		console.log('Senha Incorreta')
+	}else{
+		console.log('Senha correta')
+		document.getElementById('loginButton').disabled = false
+	}
+}
+
+const irParaProdutos = () =>{
+	
+	$.ajax({
+		async: false,
+		url: "trabalhoMarco/paginaPrincipal",
+		success: () => {
+            
+        },
+        error: (err) => {
+            console.log(err)
+        }
+	})
+}
